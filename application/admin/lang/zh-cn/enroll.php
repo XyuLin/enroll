ed03483e0234637c7590c569ffed1f5fb18edd2b
lilin <lilinphp@163.com>
2019-03-28 15:59:26 +0800

    项目初始化

---
+++ application/admin/lang/zh-cn/enroll.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Username'   => '姓名',
+    'Mobile'     => '手机号',
+    'Houses'     => '楼栋&房号',
+    'Premises'   => '楼盘',
+    'Createtime' => '报名时间',
+];
--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -3,6 +3,8 @@
 namespace app\api\controller;
 
 use app\common\controller\Api;
+use app\admin\model\Enroll;
+use think\Validate;
 
 /**
  * 首页接口
@@ -20,6 +22,45 @@ class Index extends Api
     public function index()
     {
         $this->success('请求成功');
+    }
+
+
+    public function signUp()
+    {
+        // 获取数据
+        // 判断手机号是否唯一
+        $model = new Enroll();
+        $mobile = $this->request->param('mobile/s');
+        if (!Validate::regex($mobile, "^1\d{10}$")) {
+            $this->error('手机号不规范');
+        }
+
+        $is_exist = $model->where('mobile',$mobile)->find();
+        if($is_exist) {
+            $this->error('不可重复报名!');
+        }
+
+        $count = $model->count('id');
+        if($count == 30) {
+            $this->error('活动报名人数已满!');
+        }
+
+        $params = [
+            'username'   => $this->request->param('username/s'),
+            'mobile'     => $mobile,
+            'createtime' => time(),
+            'houses'     => $this->request->param('houses/s'),
+            'premises'   => '润江城'
+        ];
+
+        
+        $result = $model->create($params);
+
+        if($result) {
+            $this->success('报名成功!');
+        } else {
+            $this->error('报名失败! 请重新提交!');
+        }
     }
 
 }
